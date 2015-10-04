5d3914392036d227f556456fe87ee7d4c8526ea1
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-10-04 08:02:05 +0700

    wt88047: TWRP-ify
    Change-Id: Id30dc0bd47be76537fc1db56cf6313ef2576ea6d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,5 +20,16 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 DEVICE_PATH := device/wingtech/wt88047
 include $(DEVICE_PATH)/board/*.mk
 
+# TWRP
+RECOVERY_VARIANT := twrp
+DEVICE_RESOLUTION := 720x1280
+TW_TARGET_USES_QCOM_BSP := true
+TW_NEW_ION_HEAP := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INCLUDE_CRYPTO := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_UNMOUNT_FIRMWARE_ON_BOOT := true
+
 # inherit from the proprietary version
 -include vendor/wingtech/wt88047/BoardConfigVendor.mk
